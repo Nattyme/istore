de174ceae9b0dddc61c35de703c4b5ce1d6c94b7
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-05-11 22:34:12 +0300

    components base data

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,7 +3,23 @@ import './style.css';
 function Main () {
     return (
         <main className="main container">
-            <aside className="sidebar">sidebar</aside>
+            <aside className="sidebar">
+                <div className="widget">
+                    <div className="widget__title">
+                        Категории
+                    </div>
+
+                    <div className="widget__body">
+                        <ul className="cat-list">
+                            <li><a href="#!">Ноутбуки</a></li>
+                            <li><a href="#!">Телефоны</a></li>
+                            <li><a href="#!">Планшеты</a></li>
+                            <li><a href="#!">Компьютеры</a></li>
+                            <li><a href="#!">Часы</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </aside>
             <div className="content">content</div>
         </main>
     )
