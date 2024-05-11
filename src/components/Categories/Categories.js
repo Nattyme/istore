dc1a1f245de1310e9a1074965c4f6e21f7d294a8
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-05-11 23:59:01 +0300

    categories base stile

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,23 @@
+import './style.css';
+
+function Categories () {
+    return (
+        <div className="widget">
+            <div className="widget__title">
+                Категории
+            </div>
+
+            <div className="widget__body">
+                <ul className="cat-list">
+                    <li><a href="#!">Ноутбуки</a></li>
+                    <li><a href="#!">Телефоны</a></li>
+                    <li><a href="#!">Планшеты</a></li>
+                    <li><a href="#!">Компьютеры</a></li>
+                    <li><a href="#!">Часы</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Categories;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,24 +1,11 @@
 import './style.css';
+import Categories from '../Categories/Categories';
 
 function Main () {
     return (
         <main className="main container">
             <aside className="sidebar">
-                <div className="widget">
-                    <div className="widget__title">
-                        Категории
-                    </div>
-
-                    <div className="widget__body">
-                        <ul className="cat-list">
-                            <li><a href="#!">Ноутбуки</a></li>
-                            <li><a href="#!">Телефоны</a></li>
-                            <li><a href="#!">Планшеты</a></li>
-                            <li><a href="#!">Компьютеры</a></li>
-                            <li><a href="#!">Часы</a></li>
-                        </ul>
-                    </div>
-                </div>
+               <Categories />
             </aside>
             <div className="content">content</div>
         </main>
