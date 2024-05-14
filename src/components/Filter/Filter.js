e6c59ad0ce57152ef719ac6aff6f8755c27a2b57
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-05-14 23:59:47 +0300

    filter base data

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -3,7 +3,21 @@ import './style.css';
 function Filter () {
     return (
         <>
-            filter
+             <div className="widget">
+            <div className="widget__title">
+                Фильтр
+            </div>
+
+            <div className="widget__body">
+                <ul className="cat-list">
+                    <li><a href="#!">Ноутбуки</a></li>
+                    <li><a href="#!">Телефоны</a></li>
+                    <li><a href="#!">Планшеты</a></li>
+                    <li><a href="#!">Компьютеры</a></li>
+                    <li><a href="#!">Часы</a></li>
+                </ul>
+            </div>
+        </div>
         </>
     );
 }
