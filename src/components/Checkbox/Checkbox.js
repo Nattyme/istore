6b9ccff89b140ce778bf201dbde90d93cbf07b2b
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-05-20 00:00:53 +0300

    checkbox base style

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -3,7 +3,8 @@ import './style.css';
 const Checkbox = (props) => {
     return (
         <div className="checkbox">
-            <input type="checkbox" className="checkbox__item"/>
+            <input type="checkbox" className="checkbox__fake"/>
+            <input type="checkbox" className="checkbox__real"/>
             <span className="checkbox__title">{props.value}</span>
         </div>
     );
