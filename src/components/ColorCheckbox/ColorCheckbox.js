927def96f82c21272e46852ad8abcc017c8e428a
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-05-25 23:57:31 +0300

    checkboxColor colors modificator styles

--- src/components/ColorCheckbox/ColorCheckbox.js
+++ src/components/ColorCheckbox/ColorCheckbox.js
@@ -3,8 +3,8 @@ import './style.css';
 const ColorCheckbox = (props) => {
     return (
         <label className="color-checkbox">
-            <input type="checkbox" className="color-checkbox__real" />
-            <span className="color-checkbox__visible"></span>
+            <input type="checkbox" className="color-checkbox__real visually-hidden " />
+            <span className="color-checkbox__visible color-checkbox__visible--white"></span>
         </label>
     );
 };
