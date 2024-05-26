c3aa1c4bf2f31273dde97b704e8fe98dd6de582c
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-05-26 22:29:15 +0300

    colorCheckbox jsx code

--- src/components/ColorCheckbox/ColorCheckbox.js
+++ src/components/ColorCheckbox/ColorCheckbox.js
@@ -4,7 +4,7 @@ const ColorCheckbox = (props) => {
     return (
         <label className="color-checkbox">
             <input type="checkbox" className="color-checkbox__real visually-hidden" />
-            <span className="color-checkbox__visible color-checkbox__visible--white"></span>
+            <span className={`color-checkbox__visible color-checkbox__visible--${props.color}`}></span>
         </label>
     );
 };
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -16,6 +16,7 @@ function Filter () {
     const colorsList = [
         'white',
         'gray',
+        'black',
         'spacegray',
         'gold',
         'blue',
