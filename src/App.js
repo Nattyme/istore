8e83b5e8248fba479869c0265c0d6e5f437db3da
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-06-14 15:37:14 +0300

    filter by price logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {Header as Header} from './components/Header/Header';
 import {Main as Main} from './components/Main/Main';
 import {Footer as Footer} from './components/Footer/Footer';
 
-import products from './data/products.json';
+import productsData from './data/products.json';
 
 function App() {
+
+    const [products, setProducts] = useState(productsData);
 
     const [filter, setFilter] = useState({
         price: {
             min: 10000,
             max: 100000,
-        }
-    })
+        },
+    });
+
+    useEffect(() => {
+        setProducts(function(){
+            return productsData.filter((product)=>{
+                if (product.price >= filter.price.min && product.price <= filter.price.max) {
+                    return true;
+                }
+            })
+        })
+    }, [filter]);
 
 
     return (
