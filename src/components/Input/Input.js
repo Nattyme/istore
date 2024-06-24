715784130ddda21503266d6996706e04595bf803
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-06-24 22:27:52 +0300

    Intl.NumberFormat for price input

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,26 +2,43 @@ import { useState as useState} from 'react';
 import './style.css';
 
 const Input = ({value, setFilter, type}) => {
-    // const [price, setPrice] = useState(props.value);
+    const formatPrice = (price) => {
+        //Создаю форматер
+        const formatter = new Intl.NumberFormat('ru-Ru', {
+            style: 'currency',
+            currency: 'RUB',
+            maximumFractionDigits: 0,
+        });
+
+        return formatter.format(price);
+    }
     
     return (
-    <input 
-    type="text" 
-    className="input" 
-    value={value} 
-    onChange={ (e) => { 
-       
-        setFilter((prev) => {
-            return {
-                ...prev,
-                price: {
-                    ...prev.price,
-                    [type]: e.target.value,
-                }
-            }
-        }) 
-    }}
-    />
+        <input 
+            type="text" 
+            className="input" 
+            value={formatPrice(value)} 
+            onChange={ (e) => { 
+            
+                setFilter((prev) => {
+                    console.log(e.target.value)
+                    let newPrice = e.target.value;
+                    newPrice = newPrice.replaceAll(' ', '');
+                    newPrice = newPrice.replaceAll(',', '.');
+                    newPrice = newPrice.replaceAll('₽', '');
+
+                    console.log(newPrice)
+
+                    return {
+                        ...prev,
+                        price: {
+                            ...prev.price,
+                            [type]: newPrice,
+                        }
+                    }
+                }) 
+            }}
+        />
     );
 }
 
